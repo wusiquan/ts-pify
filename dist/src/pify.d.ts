a52b9b658cb16ae950edc2f0ab2acace9dd5eccd
wusiquan <1781554387@qq.com>
2017-10-26 13:52:45 +0800

    m pify type annotation

--- dist/src/pify.d.ts
+++ dist/src/pify.d.ts
@@ -1,13 +1,11 @@
-export declare namespace tspify {
-    interface Opts {
-        promiseModule?: PromiseConstructor;
-        context?: any;
-    }
+export interface Opts {
+    promiseModule?: PromiseConstructor;
+    context?: any;
 }
-export declare function pify<TResult>(fn: (cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): () => Promise<TResult>;
-export declare function pify<T1, TResult>(fn: (arg1: T1, cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): (arg1: T1) => Promise<TResult>;
-export declare function pify<T1, T2, TResult>(fn: (arg1: T1, arg2: T2, cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): (arg1: T1, arg2: T2) => Promise<TResult>;
-export declare function pify<T1, T2, T3, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>;
-export declare function pify<T1, T2, T3, T4, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>;
-export declare function pify<T1, T2, T3, T4, T5, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, cb: (err: Error, res: TResult) => void) => void, setting?: tspify.Opts): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => Promise<TResult>;
+export declare function pify<TResult>(fn: (cb: (err: Error, res: TResult) => void) => void, setting?: Opts): () => Promise<TResult>;
+export declare function pify<T1, TResult>(fn: (arg1: T1, cb: (err: Error, res: TResult) => void) => void, setting?: Opts): (arg1: T1) => Promise<TResult>;
+export declare function pify<T1, T2, TResult>(fn: (arg1: T1, arg2: T2, cb: (err: Error, res: TResult) => void) => void, setting?: Opts): (arg1: T1, arg2: T2) => Promise<TResult>;
+export declare function pify<T1, T2, T3, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, cb: (err: Error, res: TResult) => void) => void, setting?: Opts): (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>;
+export declare function pify<T1, T2, T3, T4, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err: Error, res: TResult) => void) => void, setting?: Opts): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>;
+export declare function pify<T1, T2, T3, T4, T5, TResult>(fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, cb: (err: Error, res: TResult) => void) => void, setting?: Opts): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => Promise<TResult>;
 export declare function pify(fn: Function): Function;
--- src/pify.ts
+++ src/pify.ts
@@ -1,33 +1,31 @@
-export declare namespace tspify {
-  interface Opts {
-    promiseModule?: PromiseConstructor,
-    context?: any
-  }
+export interface Opts {
+  promiseModule?: PromiseConstructor,
+  context?: any
 }
 
 export function pify<TResult>(
   fn: (cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): () => Promise<TResult>
 export function pify<T1, TResult>(
   fn: (arg1: T1, cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): (arg1: T1) => Promise<TResult>
 export function pify<T1, T2, TResult>(
   fn: (arg1: T1, arg2: T2, cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): (arg1: T1, arg2: T2) => Promise<TResult>
 export function pify<T1, T2, T3, TResult>(
   fn: (arg1: T1, arg2: T2, arg3: T3, cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>
 export function pify<T1, T2, T3, T4, TResult>(
   fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>
 export function pify<T1, T2, T3, T4, T5, TResult>(
   fn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, cb: (err: Error, res: TResult) => void) => void,
-  setting?: tspify.Opts
+  setting?: Opts
 ): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => Promise<TResult>
 export function pify(fn: Function): Function
 
@@ -38,7 +36,7 @@ export function pify(fn: Function): Function
 
 // symbol flag cache?
 // option add multiArgs?
-export function pify(fn: Function, opts?: tspify.Opts): Function {
+export function pify(fn: Function, opts?: Opts): Function {
   opts = Object.assign({
     promiseModule: Promise,
     context: this
